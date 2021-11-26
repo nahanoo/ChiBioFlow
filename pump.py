85532ba9325bf16796dde0168c36f8999fd898c2
nahanaoo <eric.ulrich@unil.ch>
2021-11-26 12:08:05 +0100

    added direction option

--- pump.py
+++ pump.py
@@ -1,41 +1,67 @@
 from app import I2CCom
 import time
-from calibration import get_fit
+import pandas as pd
+from os.path import join
 
 class Reactor():
     def __init__(self,reactor_name,pump_names):
         self.reactor_name = reactor_name
         self.pumps = {pump_name:Pump(reactor_name,pump_name) for pump_name in pump_names}
-        for pump_name,pump in self.pumps.items():
-            pump.fit = get_fit(reactor_name,pump_name,5)
 
 class Pump():
     def __init__(self,reactor_name,pump_name):
-        registers = {
-            'pump1':0x06,
-            'pump2':0x0E,
-            'pump3':0x16,
-            'pump4':0x1E
+        registers = {'clock':
+            {
+                'pump1':0x06,
+                'pump2':0x0E,
+                'pump3':0x16,
+                'pump4':0x1E
+            },
+            'counter_clock':
+            {
+                'pump1':0x0A,
+                'pump2':0x12,
+                'pump3':0x1A,
+                'pump4':0x22
             }
+        }
         self.reactor_name = reactor_name
         self.pump_name = pump_name
-        self.register = registers[pump_name]
         self.running = False
-        self.fit = None
+        self.fit = self.get_fit()
+        self.direction = 'counter_clock'
+        if self.direction == 'clock':
+            self.register = registers['clock'][pump_name]
+        if self.direction == 'counter_clock':
+            self.register = registers['counter_clock'][pump_name]
+
+    def get_fit(self):
+        df = pd.read_csv(join('calibrations',self.reactor_name+'.csv'),index_col='pump_names')
+        x = 5
+        y = df.loc[self.pump_name]['after']
+        b = df.loc[self.pump_name]['before']
+        m = (y-b)/x
+        return m
 
     def turnOn(self):
-        I2CCom(self.reactor,'Pumps',0,8,self.register,1,0)
+        I2CCom(self.reactor_name,'Pumps',0,8,self.register,1,0)
         self.running = True
 
     def turnOff(self):
-        I2CCom(self.reactor,'Pumps',0,8,self.register,0,0)
+        I2CCom(self.reactor_name,'Pumps',0,8,self.register,0,0)
         self.running = False
 
+    def run_time(self,t):
+        self.turnOn()
+        time.sleep(t)
+        self.turnOff()
+
     def inject_volume(self,ml):
+        print('m',self.fit)
         runtime = ml/self.fit
         self.turnOn()
         time.sleep(runtime)
-        self.turnOff
+        self.turnOff()
 
 def create_reactors():
     pump_mappings = {
@@ -55,3 +81,6 @@ def test_pumps():
             time.sleep(0.1)
             p.turnOff()
             time.sleep(1)
+
+r = create_reactors()
+m = r['M0']
