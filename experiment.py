086223467ebc98ff06ed3799d18333517259f98f
nahanoo <eric.ulrich@unil.ch>
2021-12-03 15:05:25 +0100

    moved code to main.py

--- experiment.py
+++ experiment.py
@@ -2,26 +2,20 @@ from subprocess import call
 from os.path import exists
 from os.path import join
 from os import mkdir
-from .board import Board
+import pandas as pd
 
 
 class Experiment():
-    def __init__(self, experiment, reactor):
+    def __init__(self, experiment):
         self.experiment = experiment
-        self.reactor = reactor
-        pumps = {
-            'pump1': 'clock',
-            'pump2': 'counter_clock',
-            'pump3': 'clock',
-            'pump4': 'clock'
-        }
-        self.board = Board(pumps)
 
-    def get_data(self):
-        path = join('data', self.experiment, self.reactor)
-        if not exists(path):
-            mkdir(path)
-
+    def get_data(self,reactor):
+        path = join('data', self.experiment, reactor)
         src = 'eulrich@curnagl.dcsr.unil.ch:'+join(path, '*')
         cmd = ['scp', src, path]
         call(' '.join(cmd), shell=True)
+
+    def write_data(self,reactor):
+        path = join('data', self.experiment, reactor, 'injections')
+        if not exists(path):
+            mkdir(path)
