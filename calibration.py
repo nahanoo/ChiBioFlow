43c4cb3bd7355942bd92ccb512346d8eb2fd2bb4
nahanoo <eric.ulrich@unil.ch>
2022-02-21 10:57:45 +0100

    small script for dumping fitting parameters of od calibrations

--- calibration.py
+++ calibration.py
@@ -1,40 +1,37 @@
-from turtle import title
-import pandas as pd
 import numpy as np
 from scipy.optimize import curve_fit
 import plotly.graph_objects as go
 from os.path import join
+import csv
+import pandas as pd
 
+reactors = ['M'+str(j) for j in range(9)]
 
-ms = ['m0','m1','m3','m4','m5','m8']
-
-def mono_exp(x,m,t):
+def exp_fit(x,m,t):
     return m * np.exp(-t * x)
 
-def get_od(y,m,t):
-    return np.log(y/m)/-t
+def dump_fit(plot=False):
+    for reactor in reactors:
+        fin = join('calibrations',reactor+'_raw.csv')
+        xs = []
+        ys = []
+        df = pd.read_csv(fin,index_col=False)
+        xs += df['od'].to_list() * 3
+        for col in df.columns[1:]:
+            ys += df[col].to_list()
+        (m,t),cv = curve_fit(exp_fit,xs,ys)
+        fout = join('calibrations',reactor+'_params.csv')
+        df = pd.DataFrame(columns=['m','t'],index=[0])
+        df.at[0,'m'] = m
+        df.at[0,'t'] = t
+        df.to_csv(fout,index=False)
 
-def parse_df(f):
-    return pd.read_csv(f,index_col=False)
-
-def get_fit(f):
-    df = parse_df(f)
-    xs = 3*df['od'].to_list()
-    ys = []
-    ys += df['raw_1'].to_list()
-    ys += df['raw_2'].to_list()
-    ys += df['raw_3'].to_list()
-
-    (m,t),cv = curve_fit(mono_exp,xs,ys)
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=xs,y=ys,mode='markers'))
-    #fig.add_trace(go.Scatter(x=df['od'],y=mono_exp(df['od'],m,t),mode='lines'))
-    fig.add_trace(go.Scatter(x=np.arange(0,5.1,0.05),y=mono_exp(np.arange(0,5.1,0.05),m,t),mode='lines'))
-    fig.update_layout(
-        title=f
-    )
-    fig.show()
-
-for m in ms:
-    f = join('calibrations',m+'.csv')
-    get_fit(f)
+        if plot:
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=xs,y=ys,mode='markers'))
+            #fig.add_trace(go.Scatter(x=df['od'],y=mono_exp(df['od'],m,t),mode='lines'))
+            fig.add_trace(go.Scatter(x=np.arange(0,5.1,0.05),y=exp_fit(np.arange(0,5.1,0.05),m,t),mode='lines'))
+            fig.update_layout(
+                title=reactor
+            )
+            fig.show()
