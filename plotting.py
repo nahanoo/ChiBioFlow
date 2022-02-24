789e2b29ebe4e826b7cc726f31132e120fb8c87b
nahanoo <eric.ulrich@unil.ch>
2022-02-24 15:23:07 +0100

    added strain plot support

--- plotting.py
+++ plotting.py
@@ -1,17 +1,17 @@
 import plotly.express as px
 import argparse
-from os.path import join
-from os import listdir
+from os.path import join,split
 import pandas as pd
-import glob
-
+from glob import glob
 
 def parse_args():
     """Parsing variables for plotting."""
     parser = argparse.ArgumentParser(
         description="Plotting library for ChiBio.")
-    parser.add_argument("column", help="column name to plot from ChiBio csv.")
     parser.add_argument("experiment", help="name of the experiment directory")
+    parser.add_argument("mode",help="use 'chibio' for plotting a column of the data files; \
+        use 'strain' to plot strain composition.")
+    parser.add_argument("--column", help="column name to plot from ChiBio csv data")
     parser.add_argument(
         "--csv",
         help="path to csv if specific csv should be plotted.",
@@ -19,39 +19,85 @@ def parse_args():
     return parser.parse_args()
 
 
-def line_plot(args):
+def plot_chibio(csv=None,transfers=False):
     """Creates lineplot for parsed parameter e.g. od_measured.
     Plots every reactor as subplot. CSVs can also be parsed using
     the optional --csv flag.
     """
-    e = args.experiment
-    c = args.column
-    reactors = listdir(join("data", e))
+    reactors = [split(element)[-1] for element in glob(join("data", e,'M*'))]
     df = pd.DataFrame(columns=["exp_time", "reactor", c])
-    if args.csv is None:
+    if csv is None:
         for reactor in reactors:
-            f = glob.glob(join("data", e, reactor, "*.csv"))
+            f = glob(join("data", e, reactor, "2*.csv"))
             if len(f) > 1:
-                print(f)
                 print(
                     "There are multiple csv as sources. Clean direcotry first. Or use the optional --csv flag."
                 )
-                break
+                return
             data = pd.read_csv(f[0], usecols=["exp_time", c])
             data.insert(1, "reactor", reactor)
             # time is in seconds, deviding by 60**2 to get hours
             data["exp_time"] = data["exp_time"] / 60 / 60
             df = df.append(data)
-        fig = px.line(df, x="exp_time", y=c, facet_col="reactor",
-                      category_orders={'reactor': sorted(reactors)}, 
-                      animation_frame='exp_time',animation_group=c)
-        fig.show()
+        fig = px.line(df, x="exp_time", y=c, facet_col="reactor", facet_col_wrap=2,
+                      category_orders={'reactor': sorted(reactors)})
     else:
-        df = pd.read_csv(args.csv, usecols=["exp_time", c])
+        print(csv)
+        df = pd.read_csv(csv, usecols=["exp_time", c])
         df["exp_time"] = df["exp_time"] / 60 / 60
         fig = px.line(df, x="exp_time", y=c)
-        fig.show()
 
+    if transfers:
+        target_od = 0.12
+        for t, od in zip(df['exp_time'],df['od_measured']):
+            if od > target_od:
+                fig.add_vline(x=t)
+    fig.show()
+
+def average_cfus(series):
+    conversion = 20 * 1E2
+    dilution = [1E-1,1E-2,1E-3,1E-4,1E-5,1E-6,1E-7]
+    cfus = []
+    for counter,cfu in enumerate(series):
+        if cfu == 'overgrown':
+            pass
+        else:
+            cfu = int(cfu)
+            if cfu > 10:
+                cfus.append(cfu / dilution[counter] * conversion)
+    if len(cfus) == 0:
+        avg = 0
+    else:
+        avg = sum(cfus)/len(cfus)
+    return avg
+
+def plot_strains():
+    out = pd.DataFrame(columns=['day','reactor','at','ct','ms','oa'])
+    reactors = [split(element)[-1] for element in glob(join("data", e,'M*'))]
+    i = 0
+    for reactor in reactors:
+        for f in glob(join('data',e,reactor,'cfu*.csv')):
+            strain = f.split('.')[0][-2:]
+            df = pd.read_csv(f)
+            for day in df.columns[1:]:
+                out.at[i,'reactor'] = reactor
+                out.at[i,'day'] = day
+                out.at[i,strain] = average_cfus(df[day])
+                i += 1
+    fig = px.line(out, x="day", y=['at','ct','ms','oa'], facet_col="reactor", facet_col_wrap=2,
+                        category_orders={'reactor': sorted(reactors)})
+    fig.show()
 
 args = parse_args()
-line_plot(args)
+e = args.experiment
+c = args.column
+mode = args.mode
+if mode == 'chibio':
+    if args.csv is not None:
+        plot_chibio(args.csv)
+    else:
+        plot_chibio()
+if mode == 'strain':
+    plot_strains()
+
+
