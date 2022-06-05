a0f986a06310a97dfb00353b0b40f2460ddc1e2b
nahanoo <ericsven.u@gmail.com>
2022-06-05 15:58:05 +0200

    added triplicates and sample times

--- plotting.py
+++ plotting.py
@@ -11,11 +11,13 @@ colors = {'at': '#2c8c5a',
           'oa': '#e27e50',
           'ms': '#e5b008'}
 
+species = ['at','ct','ms','oa']
+
 names = {'at': '<i>A. tumefaciens</i>',
               'ct': '<i>C. testosteroni</i>',
               'ms': '<i>M. saperdae</i>',
               'oa': '<i>O. anthropi</i>'}
-sample_times = [19,43,67,90]
+sample_times = [19,43,67,90,109.5]
 
 
 def parse_args():
@@ -60,7 +62,7 @@ def plot_chibio(csv=None, transfers=False, sampling=True):
                           'exp_time':'',
                           'od_measured' : ''
                       })
-            fig.update_layout(font={'size':50})
+            fig.update_layout(font={'size':20})
             fig.update_layout(
             xaxis_title='Time in hours',
             yaxis_title='Measured OD')
@@ -80,8 +82,8 @@ def plot_chibio(csv=None, transfers=False, sampling=True):
                 fig.add_vline(x=t)
 
     if sampling:
-        for sample_time in sample_times:
-            fig.add_vline(x=sample_time)
+        for sample_time,day in zip(sample_times,[1,2,3,4,5]):
+            fig.add_vline(x=sample_time,annotation_text=day)
 
     fig.show()
 
@@ -126,7 +128,7 @@ def plot_strains(log=True):
                                       hovertemplate = t.hovertemplate.replace(t.name, names[t.name])
                                      )
                   )
-    fig.update_layout(font={'size':40},
+    fig.update_layout(font={'size':20},
             xaxis_title='Day',
             yaxis_title='CFUs/mL')
 
@@ -159,37 +161,72 @@ def plot_strain(log=True):
                  'pictures', 'growth_curve_' + strain + '.png')
         fig.write_image(f, scale=2)
 
-def plot_strains_triplicates(log=True):
+def plot_strains_triplicates(log=False,composition=False):
     conversion = 1E2
     out = pd.DataFrame(columns=['day', 'reactor', 'at', 'ct', 'ms', 'oa','error'])
     reactors = [split(element)[-1] for element in glob(join("data", e, 'M*'))]
     
     i = 0
+    days = []
     for reactor in reactors:
         for f in glob(join('data', e, reactor, 'cfu*.csv')):
             strain = f.split('.')[0][-2:]
             df = pd.read_csv(f)
             for day in df.columns[1:]:
-                for counter,entry in enumerate(df[day]):
-                    if '|' in str(entry):
-                        counts = [int(element) for element in entry.split('|')]
-                        counts = [10**counter*conversion*cfu for cfu in counts]
-                        cfus = statistics.mean(counts)
-                        error = statistics.stdev(counts)
-                        if cfus == 0:
-                            cfus = np.nan
-                            error = np.nan
-                out.at[i, 'reactor'] = reactor
-                out.at[i, 'day'] = day.split('_')[-1]
-                out.at[i, strain] = cfus
-                out.at[i,'error'] = error
-                i += 1
+                days.append(int(day.split('_')[-1]))
+                if day == 'day_8':
+                    pass
+                else:
+                    for counter,entry in enumerate(df[day]):
+                        if '|' in str(entry):
+                            counts = [int(element) for element in entry.split('|')]
+                            counts = [10**counter*conversion*cfu for cfu in counts]
+                            cfus = statistics.mean(counts)
+                            error = statistics.stdev(counts)
+                            if cfus == 0:
+                                cfus = np.nan
+                                error = np.nan
+                            out.at[i, 'reactor'] = reactor
+                            out.at[i, 'day'] = int(day.split('_')[-1])
+                            out.at[i, strain] = cfus
+                            out.at[i,'error'] = error
+                            i += 1
                 
     fig = px.line(out, x="day", y=['at', 'ct', 'ms', 'oa'], facet_col="reactor", facet_col_wrap=4,
                   category_orders={'reactor': sorted(reactors)}, log_y=log, color_discrete_map=colors,labels={
                     'day':''
                   },error_y='error')
+    #fig.show()
+    composition = pd.DataFrame(columns=['day', 'reactor', 'at', 'ct', 'ms', 'oa'])
+    i = 0
+    for reactor in reactors:
+        r = out[out['reactor'] == reactor]
+        for day in days:
+            d = r[r['day'] == day]
+            total = 0
+            for s in species:
+                for count in d[s]:
+                    if pd.isna(count):
+                        pass
+                    else:
+                        total += count
+            for s in species:
+                for count in d[s]:
+                    if pd.isna(count):
+                        pass
+                    else:
+                        composition.at[i,'reactor'] = reactor
+                        composition.at[i,'day'] = day
+                        composition.at[i,s] = count/total
+                        i += 1
+    composition = composition.drop_duplicates()
+    fig = px.scatter(composition, x="day", y=['at', 'ct', 'ms', 'oa'], facet_col="reactor", facet_col_wrap=4,
+                  category_orders={'reactor': sorted(reactors)},color_discrete_map=colors,labels={
+                    'day':''
+                  })
     fig.show()
+
+    
     """
     fig.for_each_trace(lambda t: t.update(name = names[t.name],
                                       legendgroup = names[t.name],
@@ -226,4 +263,4 @@ if mode == 'biofilm':
     plot_strains(csv='biofilm_cfu*.csv')
 
 if mode == 'strains_triplicates':
-    plot_strains_triplicates()
+    out = plot_strains_triplicates()
