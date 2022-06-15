e491c5247c3fe889eaf825d7ba153444bc087d7d
nahanoo <ericsven.u@gmail.com>
2022-06-15 16:27:12 +0200

    undone up or down parameter

--- app.py
+++ app.py
@@ -1281,10 +1281,7 @@ def CustomProgram(M):
 
                 if source != 'Media':
                     SetOutputOn(source,'Stir',0)
-                if Params[3] == 'up':
-                    sysData[control_reactor][pump]['target']=-1
-                if Params[3] == 'down':
-                    sysData[control_reactor][pump]['target']=1
+                sysData[control_reactor][pump]['target']=-1
                 SetOutputOn(control_reactor, pump, 1)                
                 time.sleep(run_time)
                 SetOutputOn(control_reactor, pump, 0)
