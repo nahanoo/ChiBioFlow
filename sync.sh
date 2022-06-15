e91982b20e5b6d2c47e560812079390ebf82b3b4
nahanoo <ericsven.u@gmail.com>
2022-06-15 15:39:22 +0200

    direction to be removed

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scp *.py root@192.168.7.2:/root/chibio/
-scp -r ./calibrations/ root@192.168.7.2:/root/chibio/calibrations
+#scp -r ./calibrations/ root@192.168.7.2:/root/chibio/calibrations
