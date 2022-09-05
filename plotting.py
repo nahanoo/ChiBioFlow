56ee6cf97a30ad90321febef75b7834e37586ff8
nahanoo <ericsven.u@gmail.com>
2022-09-05 18:45:56 +0200

    restructure style_plot

--- plotting.py
+++ plotting.py
@@ -11,63 +11,70 @@ import numpy as np
 from model_class import Chain
 
 
-def plot_chibio(e, order, c, multi=True):
+def plot_od(e, multi=True, model=False, chain=None):
     """Creates lineplot for parsed column e.g. od_measured.
     Plots every reactor as subplot.
     """
-    df = chibio_parser(e, order, c)
+    df, order = chibio_parser(e)
 
     if multi:
-        fig = px.line(df, x="exp_time", y=c, facet_col="reactor", color='temp', hover_data=[
+        fig = px.line(df, x="exp_time", y='od_measured', facet_col="reactor", color='temp', hover_data=[
                       'exp_time'], category_orders={'reactor': order})
 
     if not multi:
         fig = px.line(df, x="exp_time", y=c, color='temp',
-                      hover_data=['exp_time'], line_dash='reactor')
+                      hover_data=['exp_time'],)
+
+    fig = style_plot(e, fig, 'od_measured')
+
+    if model:
+        temps = df[['reactor', 'temp']].drop_duplicates()['temp'].to_list()
+        chain.experiment(int(max(df['exp_time'])))
+        for counter, f in enumerate(fig['data']):
+            xs = chain.chain[counter].xs
+            ys = chain.chain[counter].ys
+            name = str(temps[counter]) + ' model'
+            fig.add_trace(go.Scatter(x=xs, y=ys, name=name, line=dict(dash='dash')),
+                          row=1, col=counter + 1)
+        fig = style_plot(e, fig, 'od_measured')
+
     return df, fig
 
 
-def add_model(df, fig, chain):
-    temps = df[['reactor', 'temp']].drop_duplicates()['temp'].to_list()
-    chain.experiment(int(max(df['exp_time'])))
-    for counter, f in enumerate(fig['data']):
-        xs = chain.chain[counter].xs
-        ys = chain.chain[counter].ys
-        name = str(temps[counter]) + ' model'
-        fig.add_trace(go.Scatter(x=xs, y=ys, name=name),
-                      row=1, col=counter + 1)
-
-    return df, fig
-
-
-def plot_total(df, chain):
+def plot_total(e):
     """Plot sum of CFUs of all species"""
+    df, order = cfu_parser(e)
     df = df[['reactor', 'sample_time', 'total']].drop_duplicates()
     df['total'] = df['total'].replace(0, np.nan)
     fig = px.line(df, x="sample_time", y='total', facet_col="reactor",
-                  facet_col_wrap=2, category_orders={'reactor': chain}, log_y=True)
+                  facet_col_wrap=2, category_orders={'reactor': order}, log_y=True)
     fig.update_layout(font={'size': 20},
                       xaxis_title='Time in hours',
                       yaxis_title='CFUs/mL')
-    return fig
+    return df, fig
 
 
-def plot_species(df, chain):
+def plot_species(e):
     """Plots CFUs based on parsed xlsx sheet"""
+    df, order = cfu_parser(e)
     df['average'] = df['average'].replace(0, np.nan)
     fig = px.line(df, x="sample_time", y='average', facet_col="reactor",
-                  facet_col_wrap=2, category_orders={'reactor': chain}, error_y='stdev', color='species', log_y=True)
-    return fig
+                  facet_col_wrap=2, category_orders={'reactor': order}, error_y='stdev', color='species', log_y=True)
+    fig = style_plot(e, fig, 'cfus')
+    return df, fig
 
 
-def plot_composition(df, chain):
+def plot_composition(e):
     """Plots community composition in percent"""
+    df, order = cfu_parser(e)
     fig = px.line(df, x="sample_time", y='composition', facet_col="reactor",
-                  facet_col_wrap=2, category_orders={'reactor': chain}, color='species', log_y=False)
-    return fig
+                  facet_col_wrap=2, category_orders={'reactor': order}, color='species', log_y=False)
+    fig = style_plot(e, order, fig, 'composition')
+
+    return df, fig
 
 
-def style_plot(e, chain, fig, style, fontsize=20):
+def style_plot(e, fig, style, fontsize=14):
     """Updated labels and titles."""
     def species_colors(fig):
         # Species color code
@@ -116,6 +123,8 @@ def style_plot(e, chain, fig, style, fontsize=20):
 
             for sample_time in sample_times:
                 fig.add_vline(x=sample_time)
+        fig.update_layout(
+            height=350)
 
     if style == 'cfus':
         fig.for_each_xaxis(
@@ -135,7 +144,7 @@ def style_plot(e, chain, fig, style, fontsize=20):
         fig = species_names(fig)
 
     fig.update_layout(font={'size': fontsize},
-                      title='Experiment ' + e + ', order: ' + ', '.join(chain))
+                      title=e)
 
     return fig
 
@@ -161,37 +170,35 @@ def main():
     with open(join('data', e, 'order.txt'), 'r') as f:
         order = f.read().rstrip().split(',')
 
-    if (mode == 'chibio_single') | (mode == 'chibio_multi') | (mode == 'chibio_model'):
-        if mode == 'chibio_multi':
-            df, fig = plot_chibio(e, order, c)
-        if mode == 'chibio_single':
-            df, fig = plot_chibio(e, order, c, multi=False)
-        if mode == 'chibio_model':
-            df, fig = plot_chibio(e, order, c)
-            temps = df[['reactor', 'temp']].drop_duplicates()['temp'].to_list()
-            chain = Chain(temps)
-            df, fig = add_model(df, fig, chain)
-        fig = style_plot(e, order, fig, 'od_measured')
+    if mode == 'chibio_multi':
+        df, fig = plot_od(e)
+        fig.show()
+
+    if mode == 'chibio_single':
+        df, fig = plot_od(e, multi=False)
+        fig.show()
+
+    if mode == 'chibio_model':
+        df, order = chibio_parser(e)
+        temps = df[['reactor', 'temp']].drop_duplicates()['temp'].to_list()
+        chain = Chain(temps)
+        df, fig = plot_od(e, model=True, chain=chain)
+        fig.show()
         return df, fig
 
     if mode == 'species':
-        df = cfu_parser(e, chain)
-        fig = plot_species(df, chain)
-        fig = style_plot(e, chain, fig, 'cfus')
+        df, fig = plot_species(e)
         fig.show()
         return df, fig
 
     if mode == 'composition':
-        df = cfu_parser(e, chain)
-        fig = plot_composition(df, chain)
-        fig = style_plot(e, chain, fig, 'composition')
+        df, fig = plot_composition(e)
+        fig = style_plot(e, order, fig, 'composition')
         fig.show()
         return df, fig
 
     if mode == 'total':
-        df = cfu_parser(e, chain)
-        fig = plot_total(df, chain)
-        fig = style_plot(e, chain, fig, 'cfus')
+        df, fig = plot_total(e)
         fig.show()
         return df, fig
 
@@ -200,4 +207,3 @@ def main():
 
 if __name__ == '__main__':
     df, fig = main()
-    fig.show()
