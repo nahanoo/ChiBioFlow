9805021661d93e972e54b56a16403a2741b2c5e6
nahanoo <ericsven.u@gmail.com>
2022-10-17 16:15:28 +0200

    updated plotting to new model class

--- plotting.py
+++ plotting.py
@@ -11,7 +11,7 @@ from model_class import Chain
 import pandas as pd
 
 
-def plot_od(e, multi=True, model=False, chain=None):
+def plot_od_temp(e, multi=True, model=False, chain=None):
     """Creates lineplot for parsed column e.g. od_measured.
     Plots every reactor as subplot.
     """
@@ -42,6 +42,46 @@ def plot_od(e, multi=True, model=False, chain=None):
     return df, fig
 
 
+def plot_od(e, multi=True, model=False, chain=None):
+    """Creates lineplot for parsed column e.g. od_measured.
+    Plots every reactor as subplot.
+    """
+    df, order = chibio_parser(e)
+
+    if multi:
+        fig = px.line(df, x="exp_time", y='od_measured', facet_col="reactor", color='temp', hover_data=[
+                      'exp_time'], category_orders={'reactor': order}, title=None)
+        fig.update_traces(opacity=0.8)
+
+    if not multi:
+        fig = px.line(df, x="exp_time", y='od_measured', color='temp',
+                      hover_data=['exp_time'],)
+
+    fig = style_plot(e, fig, 'od_measured')
+
+    if model:
+        chain.experiment(int(max(df['exp_time'])))
+        for counter, f in enumerate(fig['data']):
+            xs = chain.xs
+            ys = chain.chain[counter].ys
+            name = chain.chain[counter].name
+            fig.add_trace(go.Scatter(x=xs, y=ys, name=name, opacity=0.8),
+                          row=1, col=counter + 1)
+        #fig = style_plot(e, fig, 'od_measured')
+
+    return df, fig
+
+
+def plot_community_model(chain):
+    dfs = []
+    for c in chain.chain:
+        df = pd.DataFrame({'x': chain.xs, 'N': c.ys, 'reactor': c.name})
+        dfs.append(df)
+    dfs = pd.concat(dfs)
+    fig = px.line(dfs, x='x', y='N', facet_col='reactor')
+    return fig
+
+
 def plot_species_model(chain):
     dfs = []
     for c in chain.chain:
@@ -60,6 +100,33 @@ def plot_species_model(chain):
         dfs.append(df)
     dfs = pd.concat(dfs)
     fig = px.line(dfs, x='x', y='N', facet_col='reactor', color='species')
+    return fig
+
+
+def plot_dilution_factors(chain):
+    dfs = []
+    for c in chain.chain:
+        df = pd.DataFrame(columns=['x', 'dilution factors',  'reactor'])
+        df['x'] = chain.x_dilutions
+        df['dilution factors'] = c.dilution_factors
+        df['reactor'] = c.name
+        dfs.append(df)
+    dfs = pd.concat(dfs)
+    fig = px.line(dfs, x='x', y='dilution factors', facet_col='reactor')
+    return fig
+
+
+def plot_carrying_capacity(chain):
+    dfs = []
+    chain.x_dilutions.insert(0,0)
+    for c in chain.chain:
+        df = pd.DataFrame(columns=['x', 'carrying capacity',  'reactor'])
+        df['x'] = chain.x_dilutions
+        df['carrying capacity'] = c.Ks
+        df['reactor'] = c.name
+        dfs.append(df)
+    dfs = pd.concat(dfs)
+    fig = px.line(dfs, x='x', y='carrying capacity', facet_col='reactor')
     return fig
 
 
@@ -82,7 +149,7 @@ def plot_species(e):
     df['average'] = df['average'].replace(0, np.nan)
     fig = px.line(df, x="sample_time", y='average', facet_col="reactor",
                   facet_col_wrap=4, category_orders={'reactor': order},
-                  error_y='stdev', color='species', log_y=True, markers=True)
+                  error_y='stdev', color='species', log_y=False, markers=True)
     fig = style_plot(e, fig, 'cfus')
     for annotation, temp in zip(fig.layout.annotations, df[['reactor', 'temp']].drop_duplicates()['temp']):
         annotation['text'] = temp
@@ -217,11 +284,18 @@ def main():
         df, fig = plot_od(e, multi=False)
         fig.show()
 
-    if mode == 'chibio_model':
+    if mode == 'chibio_model_temp':
         df, order = chibio_parser(e)
         temps = df[['reactor', 'temp']].drop_duplicates()['temp'].to_list()
         chain = Chain(temps)
-        df, fig = plot_od(e, model=True, chain=chain)
+        df, fig = plot_od_temp(e, model=True, chain=chain)
+        fig.show()
+        return df, fig
+
+    if mode == 'chibio_model':
+        df, order = chibio_parser(e)
+        chain = Chain(4)
+        df, fig = plot_od_temp(e, model=True, chain=chain)
         fig.show()
         return df, fig
 
