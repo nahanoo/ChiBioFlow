d459423df8bcb943b0a54e881909bf8f348da6ad
nahanoo <ericsven.u@gmail.com>
2023-02-03 10:30:16 +0100

    fixed growth rates

--- data/plate_reader_resources/growth_rates.py
+++ data/plate_reader_resources/growth_rates.py
@@ -1,11 +1,15 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
-
+from math import log
 df = pd.read_csv('data.csv')
 df.insert(len(df.columns), 'dNdt', None)
-df['dNdt'] = np.gradient(df['OD'], 0.5)/df['OD']
-df = df[df['Time'] >= 1]
-df = df[df['Time'] <= 74]
+df.index = range(len(df))
+wells = set(df['well'])
+for well in wells:
+    tmp = df[df['well'] == well]
+    dNdt = np.gradient(tmp['OD'], 0.5)/tmp['OD']
+    for i,j in zip(tmp.index,dNdt):
+        df.at[i,'dNdt'] = j
 fig = px.line(df, x='Time', y='dNdt', line_group='well', color='species',
               line_dash='resource')
