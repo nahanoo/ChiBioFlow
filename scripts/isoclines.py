f6419084d4c26b958bdf49a930817f32ce5062da
nahanoo <ericsven.u@gmail.com>
2025-01-31 17:18:14 +0100

    init

--- scripts/isoclines.py
+++ scripts/isoclines.py
@@ -1,8 +1,9 @@
 import numpy as np
-from scipy.integrate import odeint
 import pandas as pd
 import plotly.graph_objects as go
 from style import *
+from plotly.subplots import make_subplots
+
 
 df = dict(pd.read_csv("parameters.csv"))
 params = pd.Series(df["value"].values, index=df["parameter"]).to_dict()
@@ -13,9 +14,12 @@ width, height = 300, 250
 
 def competition():
     # Competition for acetate
-    Rs = np.linspace(0, p["M1"], 1000)
+    Rs = np.linspace(0, p["M1"] / 10, 1000)
     JCt = [p["v1_1"] * R / (R + p["K1_1"]) for R in Rs]
     JOa = [p["v2_1"] * R / (R + p["K2_1"]) for R in Rs]
+    R_star_1_1 = -p["D"] * p["K1_1"] / (p["D"] - p["v1_1"])
+    R_star_2_1 = -p["D"] * p["K2_1"] / (p["D"] - p["v2_1"])
+    Rs_2_1 = np.linspace(0, R_star_2_1, 100)
     fig = go.Figure()
     fig.add_trace(
         go.Scatter(x=Rs, y=JCt, marker=dict(color=colors["ct"]), name="<i>Ct</i>")
@@ -23,6 +27,45 @@ def competition():
     fig.add_trace(
         go.Scatter(x=Rs, y=JOa, marker=dict(color=colors["oa"]), name="<i>Oa</i>")
     )
+    fig.add_trace(
+        go.Scatter(
+            x=[0, R_star_1_1],
+            y=[p["D"], p["D"]],
+            marker=dict(color="black"),
+            line=dict(dash="dot"),
+            name="R*",
+            showlegend=True,
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=[R_star_1_1, R_star_1_1],
+            y=[0, p["D"]],
+            marker=dict(color="black"),
+            line=dict(dash="dot"),
+            showlegend=False,
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=[0, R_star_2_1],
+            y=[p["D"], p["D"]],
+            marker=dict(color="black"),
+            line=dict(dash="dot"),
+            name="R*",
+            showlegend=False,
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=[R_star_2_1, R_star_2_1],
+            y=[0, p["D"]],
+            marker=dict(color="black"),
+            line=dict(dash="dot"),
+            showlegend=False,
+        )
+    )
+
     fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="J [1/h]")
     fig.update_layout(height=height, width=width)
     fig = style_plot(fig)
@@ -59,9 +102,62 @@ def mutual_cf():
         )
     )
     fig.update_layout(height=height, width=width)
-    fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="Thiamine [mM]")
+    fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="Thiamine [µM]")
     fig = style_plot(fig)
     fig.write_image("plots/isoclines/mutual_cf.svg")
+    J_ration = np.log10(JOa / JCt)
+    custom_colorscale = [
+        [0, "blue"],  # Min value -> Blue
+        [0.5, "white"],  # Mid value (zero) -> White
+        [1, "red"],  # Max value -> Red
+    ]
+    fig = go.Figure()
+    fig.add_trace(
+        go.Contour(
+            z=J_ration,
+            x=Rs,
+            y=Ts,
+            colorscale=custom_colorscale,
+            zmid=0,
+            zmin=-0.5,
+            zmax=0.5,
+            ncontours=50,
+            contours=dict(showlines=False),
+            colorbar=dict(
+                title=dict(
+                    text="log<sub>10</sub> ( J<sub><i>Oa</i></sub> / J<sub><i>Ct</i></sub> )",
+                    side="right",
+                ),
+                len=0.5,
+            ),
+        )
+    )
+    fig.add_trace(
+        go.Contour(
+            z=JOa,
+            x=Rs,
+            y=Ts,
+            showscale=False,
+            contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
+            line=dict(color=colors["oa"]),
+            name="<i>Oa</i>",
+        )
+    )
+    fig.add_trace(
+        go.Contour(
+            z=JCt,
+            x=Rs,
+            y=Ts,
+            showscale=False,
+            contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
+            line=dict(color=colors["ct"]),
+            name="<i>Ct</i>",
+        )
+    )
+    fig.update_layout(height=height, width=width)
+    fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="Thiamine [µM]")
+    fig = style_plot(fig, font_size=8)
+    fig.write_image("plots/isoclines/mutual_cf_ratio.svg")
 
 
 def niche_creation():
@@ -102,57 +198,69 @@ def niche_creation():
     fig.write_image("plots/isoclines/niche_creation.svg")
 
 
-from plotly.subplots import make_subplots
-
-
 def niche_creation_cf():
-    pass
-
-
-Rs = np.linspace(0, 0.5, 100)
-Ms = np.linspace(0, 0.5, 100)
-Ts = np.linspace(0, p["M3"] / 10, 100)
-Ct_R_grid, Ct_M_grid = np.meshgrid(Rs, Ms)
-Oa_R_grid, Oa_T_grid = np.meshgrid(Rs, Ts)
-JCt = p["v1_1"] * Ct_R_grid / (Ct_R_grid + p["K1_1"]) + p["v1_2"] * Ct_M_grid / (
-    Ct_M_grid + p["K1_2"]
-)
-JOa = (
-    p["v2_1"]
-    * Oa_R_grid
-    / (Oa_R_grid + p["K2_1"])
-    * Oa_T_grid
-    / (Oa_T_grid + p["K2_3"])
-)
-
-fig = make_subplots(specs=[[{"secondary_y": True}]])
-fig.add_trace(
-    go.Contour(
-        z=JOa,
-        x=Rs,
-        y=Ts,
-        showscale=False,
-        contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
-        line=dict(color=colors["oa"]),
-        name="<i>Oa</i>",
+    Rs = np.linspace(0, 0.5, 10)
+    Ms = np.linspace(0, 0.5, 10)
+    Ts = np.linspace(0, p["M3"] / 20, 100)
+    R_grid, M_grid, T_grid = np.meshgrid(Rs, Ms, Ts)
+    JCt = p["v1_1"] * R_grid / (R_grid + p["K1_1"]) + p["v1_2"] * M_grid / (
+        M_grid + p["K1_2"]
     )
-)
-fig.add_trace(
-    go.Contour(
-        z=JCt,
-        x=Rs,
-        y=Ms,
-        showscale=False,
-        contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
-        line=dict(color=colors["ct"]),
-        name="<i>Ct</i>",
-    ),
-    secondary_y=True,
-)
-fig.update_layout(height=height, width=width)
-fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="Metabolite [mM]")
-fig = style_plot(fig)
-fig.write_image("plots/isoclines/niche_creation_cf.svg")
+    JOa = p["v2_1"] * R_grid / (R_grid + p["K2_1"]) * T_grid / (T_grid + p["K2_3"])
 
-
-niche_creation_cf()
+    fig = go.Figure()
+    fig.add_trace(
+        go.Isosurface(
+            x=R_grid.flatten(),
+            y=M_grid.flatten(),
+            z=T_grid.flatten(),
+            value=JCt.flatten(),
+            isomin=0.15,
+            isomax=0.15,
+            surface_count=1,
+            colorscale=[[0, colors["ct"]], [1, colors["ct"]]],
+            name="<i>Ct</i>",
+            showscale=False,
+            showlegend=True,
+            opacity=0.5,
+        )
+    )
+    fig.add_trace(
+        go.Isosurface(
+            x=R_grid.flatten(),
+            y=M_grid.flatten(),
+            z=T_grid.flatten(),
+            value=JOa.flatten(),
+            isomin=0.15,
+            isomax=0.15,
+            surface_count=1,
+            colorscale=[[0, colors["oa"]], [1, colors["oa"]]],
+            name="<i>Oa</i>",
+            showlegend=True,
+            showscale=False,
+            opacity=0.5,
+        ),
+    )
+    fig.add_trace(
+        go.Scatter3d(
+            x=[0.08 + 0.02],
+            y=[0.019],
+            z=[0.22],
+            marker=dict(color="black", opacity=1),
+            showlegend=False,
+        )
+    )
+    fig.update_layout(
+        scene=dict(
+            zaxis=dict(title="Thamine [µM]"),
+            xaxis=dict(title="Acetate [mM]"),
+            yaxis=dict(title="Metabolite [mM]"),
+        )
+    )
+    fig.update_layout(
+        height=height,
+        width=width,
+        scene_camera=dict(eye=dict(x=2.0, y=2.0, z=2), center=dict(x=0.3, y=0, z=-0.2)),
+    )
+    fig = style_plot(fig, top_margin=0, left_margin=0, buttom_margin=0, font_size=7)
+    fig.write_image("plots/isoclines/niche_creation_cf.svg")
--- scripts/models.py
+++ scripts/models.py
@@ -1,165 +1,317 @@
 import numpy as np
 from scipy.integrate import odeint
+import pandas as pd
+from style import *
+import plotly.graph_objects as go
 
-#xs = np.linspace(0, 10000, 10000)
+df = dict(pd.read_csv("parameters.csv"))
+params = pd.Series(df["value"].values, index=df["parameter"]).to_dict()
+p = params
+
+width, height = 2 * 300, 2 * 250
+xs = np.linspace(0, 2000, 2000)
 
 
-def species_1_niche_1(t, f_dN1_1, f_dR1_1, params):
+def ct_mono():
+    def model(y, t):
+        Ct, R = y
+        JCt = p["v1_1"] * R / (R + p["K1_1"])
+        dCt = JCt * Ct - p["D"] * Ct
+        dR = -JCt * Ct / p["q1_1"] - p["D"] * R + p["D"] * p["M1"]
+        return dCt, dR
 
-    def model(y, t, f_dN1_1, f_dR1_1, params):
-        N1, R1 = y
-        return [
-            f_dN1_1(N1, R1, *params.values()),
-            f_dR1_1(N1, R1, *params.values())
-        ]
-
-    return odeint(
-        model,
-        [params["N01"], params["M1"]],
-        t,
-        args=(f_dN1_1, f_dR1_1, params),
+    Y = odeint(model, [p["N01"], p["M1"]], xs)
+    Ct, R = Y[-1]
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=xs,
+            y=Y[:, 0],
+            marker=dict(color=colors["ct"]),
+            opacity=0.5,
+            name="<i>Ct</i> monoculture",
+        )
     )
+    return fig
 
 
-def species_1_niche_2(t, f_dN1_2, f_dR1_2, params):
+def oa_mono():
+    def model(y, t):
+        Oa, R = y
+        JOa = p["v2_1"] * R / (R + p["K2_1"])
+        dOa = JOa * Oa - p["D"] * Oa
+        dR = -JOa * Oa / p["q2_1"] - p["D"] * R + p["D"] * p["M1"]
+        return dOa, dR
 
-    def model(y, t, f_dN1_2, f_dR1_2, params):
-        N1, R2 = y
-        return [
-            f_dN1_2(N1, R2, *params.values()),
-            f_dR1_2(N1, R2, *params.values()),
-        ]
-
-    return odeint(
-        model,
-        [params["N01"], params["M2"]],
-        t,
-        args=(f_dN1_2, f_dR1_2, params),
+    Y = odeint(model, [p["N02"], p["M1"]], xs)
+    Oa, R = Y[-1]
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=xs,
+            y=Y[:, 0],
+            marker=dict(color=colors["oa"]),
+            opacity=0.5,
+            name="<i>Oa</i> monoculture",
+        )
     )
+    return fig
 
 
-def species_2_niche_1(t, f_dN2_1, f_dR2_1, params):
+def competition():
+    def model(y, t):
+        Ct, Oa, R = y
+        JCt = p["v1_1"] * R / (R + p["K1_1"])
+        JOa = p["v2_1"] * R / (R + p["K2_1"])
+        dCt = JCt * Ct - p["D"] * Ct
+        dOa = JOa * Oa - p["D"] * Oa
+        dR = (
+            -JCt * Ct / p["q1_1"] - JOa * Oa / p["q2_1"] + p["D"] * p["M1"] - p["D"] * R
+        )
+        return dCt, dOa, dR
 
-    def model(y, t, f_dN2_1, f_dR2_1, params):
-        N2, R1 = y
-        return [
-            f_dN2_1(N2, R1, *params.values()),
-            f_dR2_1(N2, R1, *params.values()),
-        ]
+    Y = odeint(model, [p["N01"], p["N02"], p["M1"]], xs)
+    Ct, Oa, R = Y[-1]
+    print("Ct", Ct, "Oa", Oa, "R", R)
 
-    return odeint(
-        model,
-        [params["N02"], params["M1"]],
-        t,
-        args=(f_dN2_1, f_dR2_1, params),
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=xs, y=Y[:, 0], name="<i>Ct</i>", marker=dict(color=colors["ct"]))
     )
-
-
-def species_2_niche_2(t, f_dN2_2, f_dR2_2, params):
-
-    def model(y, t, f_dN2_2, f_dR2_2, params):
-        N2, R2 = y
-        return [
-            f_dN2_2(N2, R2, *params.values()),
-            f_dR2_2(N2, R2, *params.values()),
-        ]
-
-    return odeint(
-        model,
-        [params["N02"], params["M2"]],
-        t,
-        args=(f_dN2_2, f_dR2_2, params),
+    fig.add_trace(
+        go.Scatter(x=xs, y=Y[:, 1], name="<i>Oa</i>", marker=dict(color=colors["oa"]))
     )
+    ct_mono_trace = ct_mono()
+    oa_mono_trace = oa_mono()
+    fig.add_trace(ct_mono_trace.data[0])
+    fig.add_trace(oa_mono_trace.data[0])
+    fig.update_xaxes(title="Time [h]"), fig.update_yaxes(title="OD")
+    fig.update_layout(width=width, height=height)
+    fig = style_plot(fig)
+    fig.write_image("plots/simulations/competition.svg")
 
 
-def species_12_niche_1(t, f_dN1_1, f_dN2_1, f_dR12_1, params):
+def model(y, t):
+    Ct, Oa, R, T = y
+    JCt = p["v1_1"] * R / (R + p["K1_1"])
+    JOa = p["v2_1"] * R / (R + p["K2_1"]) * T / (T + p["K2_3"])
+    dCt = JCt * Ct - p["D"] * Ct
+    dOa = JOa * Oa - p["D"] * Oa
+    dR = -JCt * Ct / p["q1_1"] - JOa * Oa / p["q2_1"] + p["D"] * p["M1"] - p["D"] * R
+    dT = -JOa * Oa / p["q2_3"] - p["D"] * T + p["M3"] * p["D"] + JCt * Ct / p["a1_3"]
+    return dCt, dOa, dR, dT
 
-    def model(y, t, f_dN1_1, f_dN2_1, f_dR12_1, params):
-        N1, N2, R1 = y
-        return [
-            f_dN1_1(N1, R1, *params.values()),
-            f_dN2_1(N2, R1, *params.values()),
-            f_dR12_1(N1, N2, R1, *params.values()),
-        ]
 
-    return odeint(
-        model,
-        [params["N01"], params["N02"], params["M1"]],
-        t,
-        args=(f_dN1_1, f_dN2_1, f_dR12_1, params),
+Ms = np.linspace(0, 7.5, 100)
+Ts = np.linspace(0, 10, 100)
+ratios = []
+for M in Ms:
+    alpha_ratios = []
+    for T in Ts:
+        p["M1"] = M
+        p["M3"] = T
+        Y = odeint(model, [p["N01"], p["N02"], p["M1"], p["M3"]], xs)
+        Ct, Oa, R, T = Y[-1]
+        if (Ct < 1e-3) & (Oa < 1e-3):
+            alpha_ratios.append(None)
+        elif (Ct < 1e-3) & (Oa > 1e-3):
+            alpha_ratios.append(1)
+        elif (Ct > 1e-3) & (Oa < 1e-3):
+            alpha_ratios.append(0)
+        else:
+            alpha_ratios.append(Oa / (Ct + Oa))
+    ratios.append(alpha_ratios)
+custom_colorscale = [
+    [0, "blue"],  # Min value -> Blue
+    [0.5, "white"],  # Mid value (zero) -> White
+    [1, "red"],  # Max value -> Red
+]
+fig = go.Figure()
+fig.add_trace(
+    go.Contour(
+        z=np.array(ratios),
+        x=Ts,
+        y=Ms,
+        colorscale=custom_colorscale,
+        ncontours=50,
+        contours=dict(showlines=False),
+        colorbar=dict(
+            title=dict(
+                text="<i>Oa</i> fraction",
+                side="right",
+            ),
+            len=0.5,
+        ),
     )
+)
+fig.write_image("plots/simulations/supp_thiamine_ratios.svg")
 
 
-def species_12_niche_2(t, f_dN1_2, f_dN2_2, f_dR12_2, params):
+def mutual_cf():
+    def model(y, t):
+        Ct, Oa, R, T = y
+        JCt = p["v1_1"] * R / (R + p["K1_1"])
+        JOa = p["v2_1"] * R / (R + p["K2_1"]) * T / (T + p["K2_3"])
+        dCt = JCt * Ct - p["D"] * Ct
+        dOa = JOa * Oa - p["D"] * Oa
+        dR = (
+            -JCt * Ct / p["q1_1"] - JOa * Oa / p["q2_1"] + p["D"] * p["M1"] - p["D"] * R
+        )
+        dT = JCt * Ct / p["a1_3"] - JOa * Oa * p["q2_3"] - p["D"] * T
+        return dCt, dOa, dR, dT
 
-    def model(y, t, f_dN1_2, f_dN2_2, f_dR12_2, params):
-        N1, N2, R2 = y
-        return [
-            f_dN1_2(N1, R2, *params.values()),
-            f_dN2_2(N2, R2, *params.values()),
-            f_dR12_2(N1, N2, R2, *params.values()),
-        ]
-
-    return odeint(
-        model,
-        [params["N01"], params["N02"], params["M2"]],
-        t,
-        args=(f_dN1_2, f_dN2_2, f_dR12_2, params),
+    Y = odeint(model, [p["N01"], p["N02"], p["M1"], 0], xs)
+    Ct, Oa, R, T = Y[-1]
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=xs, y=Y[:, 0], name="<i>Ct</i>", marker=dict(color=colors["ct"]))
     )
-
-
-def species_12_niche_12(t, f_dN1_1, f_dN2_2, f_dR1_1, f_dR2_2, params):
-
-    def model(y, t, f_dN1_1, f_dN2_2, f_dR1_1, f_dR2_2, params):
-        N1, N2, R1, R2 = y
-        return [
-            f_dN1_1(N1, R1, *params.values()),
-            f_dN2_2(N2, R2, *params.values()),
-            f_dR1_1(N1, R1, *params.values()),
-            f_dR2_2(N2, R2, *params.values()),
-        ]
-
-    return odeint(
-        model,
-        [params["N01"], params["N02"], params["M1"], params["M2"]],
-        t,
-        args=(f_dN1_1, f_dN2_2, f_dR1_1, f_dR2_2, params),
+    fig.add_trace(
+        go.Scatter(x=xs, y=Y[:, 1], name="<i>Oa</i>", marker=dict(color=colors["oa"]))
     )
+    ct_mono_trace = ct_mono()
+    oa_mono_trace = oa_mono()
+    fig.add_trace(ct_mono_trace.data[0])
+    fig.add_trace(oa_mono_trace.data[0])
+    fig.update_xaxes(title="Time [h]"), fig.update_yaxes(title="OD")
+    fig.update_layout(width=width, height=height)
+    fig = style_plot(fig)
+    fig.write_image("plots/simulations/mutual_cf.svg")
 
 
-def species_12_niche_21(t, f_dN1_2, f_dN2_1, f_dR1_2, f_dR2_1, params):
+def cf_mutual_species_ratio():
+    def model(y, t):
+        Ct, Oa, R, T = y
+        JCt = p["v1_1"] * R / (R + p["K1_1"])
+        JOa = p["v2_1"] * R / (R + p["K2_1"]) * T / (T + p["K2_3"])
+        dCt = JCt * Ct - p["D"] * Ct
+        dOa = JOa * Oa - p["D"] * Oa
+        dR = (
+            -JCt * Ct / p["q1_1"] - JOa * Oa / p["q2_1"] + p["D"] * p["M1"] - p["D"] * R
+        )
+        dT = JCt * Ct * p["q1_3"] - JOa * Oa * p["q2_3"] - p["D"] * T
+        return dCt, dOa, dR, dT
 
-    def model(y, t, f_dN1_2, f_dN2_1, f_dR1_2, f_dR2_1, params):
-        N1, N2, R1, R2 = y
-        return [
-            f_dN1_2(N1, R2, *params.values()),
-            f_dN2_1(N2, R1, *params.values()),
-            f_dR2_1(N2, R1, *params.values()),
-            f_dR1_2(N1, R2, *params.values()),
-        ]
-
-    return odeint(
-        model,
-        [params["N01"], params["N02"], params["M1"], params["M2"]],
-        t,
-        args=(f_dN1_2, f_dN2_1, f_dR1_2, f_dR2_1, params),
+    Ms = np.linspace(0, 7.5, 100)
+    alphas = np.linspace(0, 5, 100)
+    ratios = []
+    for M in Ms:
+        alpha_ratios = []
+        for alpha in alphas:
+            p["M1"] = M
+            p["q1_3"] = alpha
+            Y = odeint(model, [p["N01"], p["N02"], p["M1"], 0], xs)
+            Ct, Oa, R, T = Y[-1]
+            if (Ct < 1e-3) & (Oa < 1e-3):
+                alpha_ratios.append(None)
+            else:
+                alpha_ratios.append(Oa / Ct)
+        ratios.append(alpha_ratios)
+    custom_colorscale = [
+        [0, "blue"],  # Min value -> Blue
+        [0.5, "white"],  # Mid value (zero) -> White
+        [1, "red"],  # Max value -> Red
+    ]
+    fig = go.Figure()
+    fig.add_trace(
+        go.Contour(
+            z=np.array(ratios),
+            x=alphas,
+            y=Ms,
+            colorscale=custom_colorscale,
+            ncontours=50,
+            contours=dict(showlines=False),
+            colorbar=dict(
+                title=dict(
+                    text="<i>Oa</i> fraction",
+                    side="right",
+                ),
+                len=0.5,
+            ),
+        )
     )
+    fig.write_image("plots/simulations/mutual_cf_ratios.svg")
 
 
-def species_12_niche_13(t, f_dN1_1, f_dN2_13, f_dR12_13, f_dR2_3, params):
+def niche_creation():
+    def model(y, t):
+        Ct, Oa, R, M = y
+        JCtR = p["v1_1"] * R / (R + p["K1_1"])
+        JCtM = p["v1_2"] * M / (M + p["K1_2"])
+        JOa = p["v2_1"] * R / (R + p["K2_1"])
+        dCt = JCtR * Ct + JCtM * Ct - p["D"] * Ct
+        dOa = JOa * Oa - p["D"] * Oa
+        dR = (
+            -JCtR * Ct / p["q1_1"]
+            - JOa * Oa / p["q2_1"]
+            - p["D"] * R
+            + p["D"] * p["M1"]
+        )
+        dM = JOa * Oa / p["a2_2"] - JCtM * Ct / p["q1_2"] - p["D"] * M
+        return dCt, dOa, dR, dM
 
-    def model(y, t, f_dN1_1, f_dN2_13, f_dR12_13, f_dR2_3, params):
-        N1, N2, R1, R3 = y
-        return [
-            f_dN1_1(N1, R1, *params.values()),
-            f_dN2_13(N2, R1, R3, *params.values()),
-            f_dR12_13(N1, N2, R1, *params.values()),
-            f_dR2_3(N1, N2, R1, R3, *params.values()),
-        ]
-
-    return odeint(
-        model,
-        [params["N01"], params["N02"], params["M1"], params["M3"]],
-        t,
-        args=(f_dN1_1, f_dN2_13, f_dR12_13, f_dR2_3, params),
+    Y = odeint(model, [p["N01"], p["N02"], p["M1"], 0], xs)
+    Ct, Oa, R, M = Y[-1]
+    print("Ct", Ct, "Oa", Oa, "R", R, "M", M)
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=xs, y=Y[:, 0], name="<i>Ct</i>", marker=dict(color=colors["ct"]))
     )
+    fig.add_trace(
+        go.Scatter(x=xs, y=Y[:, 1], name="<i>Oa</i>", marker=dict(color=colors["oa"]))
+    )
+    Ct, Oa, R, M = Y[-1]
+    ct_mono_trace = ct_mono()
+    oa_mono_trace = oa_mono()
+    fig.add_trace(ct_mono_trace.data[0])
+    fig.add_trace(oa_mono_trace.data[0])
+    fig.update_xaxes(title="Time [h]"), fig.update_yaxes(title="OD")
+    fig.update_layout(width=width, height=height)
+    fig = style_plot(fig)
+    fig.write_image("plots/simulations/niche_creation.svg")
+
+
+def niche_creation_cf():
+    def model(y, t):
+        Ct, Oa, R, T, M = y
+        JCtR = p["v1_1"] * R / (R + p["K1_1"])
+        JCtM = p["v1_2"] * M / (M + p["K1_2"])
+        JOa = p["v2_1"] * R / (R + p["K2_1"]) * T / (T + p["K2_3"])
+        dCt = JCtR * Ct + JCtM * Ct - p["D"] * Ct
+        dOa = JOa * Oa - p["D"] * Oa
+        dR = (
+            -JCtR * Ct / p["q1_1"]
+            - JOa * Oa / p["q2_1"]
+            - p["D"] * R
+            + p["D"] * p["M1"]
+        )
+        dT = (
+            JCtM * Ct / p["a1_3"]
+            + JCtR * Ct / p["a1_3"]
+            - JOa * Oa / p["q2_3"]
+            - p["D"] * T
+        )
+        dM = JOa * Oa / p["a2_2"] - JCtM * Ct / p["q1_2"] - p["D"] * M
+        return dCt, dOa, dR, dT, dM
+
+    Y = odeint(model, [p["N01"], p["N02"], p["M1"], 0, 0], xs)
+    Ct, Oa, R, T, M = Y[-1]
+    print("Ct", Ct, "Oa", Oa, "R", R, "T", T, "M", M)
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(x=xs, y=Y[:, 0], name="<i>Ct</i>", marker=dict(color=colors["ct"]))
+    )
+    fig.add_trace(
+        go.Scatter(x=xs, y=Y[:, 1], name="<i>Oa</i>", marker=dict(color=colors["oa"]))
+    )
+    ct_mono_trace = ct_mono()
+    oa_mono_trace = oa_mono()
+    fig.add_trace(ct_mono_trace.data[0])
+    fig.add_trace(oa_mono_trace.data[0])
+    Ct, Oa, R, T, M = Y[-1]
+    fig.update_xaxes(title="Time [h]"), fig.update_yaxes(title="OD")
+    fig.update_layout(width=width, height=height)
+    fig = style_plot(fig)
+    fig.write_image("plots/simulations/niche_creation_cf.svg")
+    return R, M
