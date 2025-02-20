602a3c4f539c9321b1a7d353db527f5a50334f5c
nahanoo <ericsven.u@gmail.com>
2025-02-20 10:57:47 +0100

    polished code draft version of paper figures

--- scripts/isoclines.py
+++ scripts/isoclines.py
@@ -10,112 +10,35 @@ params = pd.Series(df["value"].values, index=df["parameter"]).to_dict()
 p = params
 
 width, height = 300, 250
+lm = 10
+bm = 10
+tm = 10
+rm = 10
+font_size = 8
+line_thickness = 1.2
+Rs = np.linspace(1e-10, 0.5, 100)
 
 
 def competition():
     # Competition for acetate
-    Rs = np.linspace(0, p["M1"] / 10, 1000)
     JCt = [p["v1_1"] * R / (R + p["K1_1"]) for R in Rs]
     JOa = [p["v2_1"] * R / (R + p["K2_1"]) for R in Rs]
-    R_star_1_1 = -p["D"] * p["K1_1"] / (p["D"] - p["v1_1"])
-    R_star_2_1 = -p["D"] * p["K2_1"] / (p["D"] - p["v2_1"])
-
-    fig = go.Figure()
-    fig.add_trace(
-        go.Scatter(x=Rs, y=JCt, marker=dict(color=colors["ct"]), name="<i>Ct</i>")
-    )
-    fig.add_trace(
-        go.Scatter(x=Rs, y=JOa, marker=dict(color=colors["oa"]), name="<i>Oa</i>")
-    )
-    fig.add_trace(
-        go.Scatter(
-            x=[0, R_star_1_1],
-            y=[p["D"], p["D"]],
-            marker=dict(color="black"),
-            line=dict(dash="dot"),
-            name="R*",
-            showlegend=True,
-        )
-    )
-    fig.add_trace(
-        go.Scatter(
-            x=[R_star_1_1, R_star_1_1],
-            y=[0, p["D"]],
-            marker=dict(color="black"),
-            line=dict(dash="dot"),
-            showlegend=False,
-        )
-    )
-    fig.add_trace(
-        go.Scatter(
-            x=[0, R_star_2_1],
-            y=[p["D"], p["D"]],
-            marker=dict(color="black"),
-            line=dict(dash="dot"),
-            name="R*",
-            showlegend=False,
-        )
-    )
-    fig.add_trace(
-        go.Scatter(
-            x=[R_star_2_1, R_star_2_1],
-            y=[0, p["D"]],
-            marker=dict(color="black"),
-            line=dict(dash="dot"),
-            showlegend=False,
-        )
-    )
-
-    fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="J [1/h]")
-    fig.update_layout(height=height, width=width)
-    fig = style_plot(fig)
-    fig.write_image("plots/isoclines/competition.svg")
-
-
-def mutual_cf():
-    Rs = np.linspace(0, 0.5, 1000)
-    Ts = np.linspace(0, p["M3"] / 10, 1000)
-    R_grid, T_grid = np.meshgrid(Rs, Ts)
-    JCt = p["v1_1"] * R_grid / (R_grid + p["K1_1"])
-    JOa = p["v2_1"] * R_grid / (R_grid + p["K2_1"]) * T_grid / (T_grid + p["K2_3"])
-    fig = go.Figure()
-    fig.add_trace(
-        go.Contour(
-            z=JOa,
-            x=Rs,
-            y=Ts,
-            showscale=False,
-            contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
-            line=dict(color=colors["oa"]),
-            name="<i>Oa</i>",
-        )
-    )
-    fig.add_trace(
-        go.Contour(
-            z=JCt,
-            x=Rs,
-            y=Ts,
-            showscale=False,
-            contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
-            line=dict(color=colors["ct"]),
-            name="J <i>Ct</i>",
-        )
-    )
-    fig.update_layout(height=height, width=width)
-    fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="Thiamine [µM]")
-    fig.write_image("plots/isoclines/mutual_cf.svg")
-    J_ration = np.log10(JOa / JCt)
+    Ds = np.linspace(0, max(JCt), 100)
+    R_grid, D_grid = np.meshgrid(Rs, Ds)
+    JCt_grid = p["v1_1"] * R_grid / (R_grid + p["K1_1"])
+    JOa_grid = p["v2_1"] * R_grid / (R_grid + p["K2_1"])
+    R_ratio = np.log(JOa_grid / JCt_grid)
     custom_colorscale = [
-        [0, "blue"],  # Min value -> Blue
+        [0, "lightblue"],  # Min value -> Blue
         [0.5, "white"],  # Mid value (zero) -> White
-        [1, "red"],  # Max value -> Red
+        [1, "lightsalmon"],  # Max value -> Red
     ]
     fig = go.Figure()
     fig.add_trace(
         go.Contour(
-            z=J_ration,
+            z=R_ratio,
             x=Rs,
-            y=Ts,
+            y=Ds,
             colorscale=custom_colorscale,
             zmid=0,
             zmin=-0.5,
@@ -128,18 +51,68 @@ def mutual_cf():
                     side="right",
                 ),
                 len=0.5,
+                y=0.4,
             ),
         )
     )
     fig.add_trace(
+        go.Scatter(
+            x=Rs, y=JCt, marker=dict(color=colors["ct"]), name="<i>Ct</i>", opacity=1
+        )
+    )
+    fig.add_trace(
+        go.Scatter(x=Rs, y=JOa, marker=dict(color=colors["oa"]), name="<i>Oa</i>")
+    )
+    fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="J [1/h]")
+    fig.update_layout(height=height, width=width)
+    fig.update_layout(legend_title_text="Isocline of<br>growth rate")
+    fig.update_layout(
+        xaxis=dict(showgrid=False, ticks="outside"),
+        yaxis=dict(showgrid=False, ticks="outside"),
+    )
+    fig = style_plot(
+        fig,
+        line_thickness=line_thickness,
+        font_size=font_size,
+        left_margin=lm,
+        buttom_margin=bm,
+        top_margin=tm,
+        right_margin=rm,
+    )
+    fig.write_image("plots/isoclines/competition.svg")
+
+
+def mutual_cf():
+    Ts = np.linspace(1e-10, p["M3"] / 10, 100)
+    R_grid, T_grid = np.meshgrid(Rs, Ts)
+    JCt = p["v1_1"] * R_grid / (R_grid + p["K1_1"])
+    JOa = p["v2_1"] * R_grid / (R_grid + p["K2_1"]) * T_grid / (T_grid + p["K2_3"])
+    J_ratio = np.log10(JOa / JCt)
+    custom_colorscale = [
+        [0, "lightblue"],  # Min value -> Blue
+        [0.5, "white"],  # Mid value (zero) -> White
+        [1, "lightsalmon"],  # Max value -> Red
+    ]
+    fig = go.Figure()
+    fig.add_trace(
         go.Contour(
-            z=JOa,
+            z=J_ratio,
             x=Rs,
             y=Ts,
-            showscale=False,
-            contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
-            line=dict(color=colors["oa"]),
-            name="<i>Oa</i>",
+            colorscale=custom_colorscale,
+            zmid=0,
+            zmin=-0.1,
+            zmax=0.1,
+            ncontours=50,
+            contours=dict(showlines=False),
+            colorbar=dict(
+                title=dict(
+                    text="log<sub>10</sub> ( J<sub><i>Oa</i></sub> / J<sub><i>Ct</i></sub> )",
+                    side="right",
+                ),
+                len=0.5,
+                y=0.4,
+            ),
         )
     )
     fig.add_trace(
@@ -153,43 +126,73 @@ def mutual_cf():
             name="<i>Ct</i>",
         )
     )
-    fig.update_layout(height=height, width=width)
+    fig.add_trace(
+        go.Contour(
+            z=JOa,
+            x=Rs,
+            y=Ts,
+            showscale=False,
+            contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
+            line=dict(color=colors["oa"]),
+            name="<i>Oa</i>",
+        )
+    )
+
+    fig.update_layout(
+        height=height,
+        width=width,
+        legend_title_text="Isocline of<br>growth rate",
+        xaxis=dict(showgrid=False, ticks="outside"),
+        yaxis=dict(showgrid=False, ticks="outside"),
+    )
     fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="Thiamine [µM]")
     fig = style_plot(
         fig,
-        font_size=8,
-        line_thickness=1.5,
-        buttom_margin=20,
-        left_margin=30,
-        right_margin=20,
-        top_margin=0,
+        line_thickness=line_thickness,
+        font_size=font_size,
+        left_margin=lm,
+        buttom_margin=bm,
+        top_margin=tm,
+        right_margin=rm,
     )
-    fig.update_layout(legend_title_text="Isocline of<br>growth rate")
-    fig.write_image("plots/isoclines/mutual_cf_ratio.svg")
-
-
-mutual_cf()
+    fig.write_image("plots/isoclines/mutual_cf.svg")
 
 
 def niche_creation():
-    Rs = np.linspace(0, 0.5, 1000)
-    Ms = np.linspace(0, 0.5, 1000)
+    Rs = np.linspace(1e-10, 0.5, 1000)
+    Ms = np.linspace(1e-10, 0.1, 1000)
     R_grid, M_grid = np.meshgrid(Rs, Ms)
     JCt = p["v1_1"] * R_grid / (R_grid + p["K1_1"]) + p["v1_2"] * M_grid / (
         M_grid + p["K1_2"]
     )
     JOa = p["v2_1"] * R_grid / (R_grid + p["K2_1"])
 
+    J_ratio = np.log10(JOa / JCt)
+    custom_colorscale = [
+        [0, "lightblue"],  # Min value -> Blue
+        [0.5, "white"],  # Mid value (zero) -> White
+        [1, "lightsalmon"],  # Max value -> Red
+    ]
     fig = go.Figure()
     fig.add_trace(
         go.Contour(
-            z=JOa,
+            z=J_ratio,
             x=Rs,
             y=Ms,
-            showscale=False,
-            contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
-            line=dict(color=colors["oa"]),
-            name="<i>Oa</i>",
+            colorscale=custom_colorscale,
+            zmid=0,
+            zmin=-0.1,
+            zmax=0.1,
+            ncontours=50,
+            contours=dict(showlines=False),
+            colorbar=dict(
+                title=dict(
+                    text="log<sub>10</sub> ( J<sub><i>Oa</i></sub> / J<sub><i>Ct</i></sub> )",
+                    side="right",
+                ),
+                len=0.5,
+                y=0.4,
+            ),
         )
     )
     fig.add_trace(
@@ -203,39 +206,53 @@ def niche_creation():
             name="<i>Ct</i>",
         )
     )
-    fig.update_layout(height=height, width=width)
+    fig.add_trace(
+        go.Contour(
+            z=JOa,
+            x=Rs,
+            y=Ms,
+            showscale=False,
+            contours=dict(start=0.15, end=0.15, size=0.1, coloring="none"),
+            line=dict(color=colors["oa"]),
+            name="<i>Oa</i>",
+        )
+    )
+    fig.update_layout(
+        height=height,
+        width=width,
+        legend_title_text="Isocline of<br>growth rate",
+        xaxis=dict(showgrid=False, ticks="outside"),
+        yaxis=dict(showgrid=False, ticks="outside"),
+    )
     fig.update_xaxes(title="Acetate [mM]"), fig.update_yaxes(title="Metabolite [mM]")
-    fig = style_plot(fig)
+    fig = style_plot(
+        fig,
+        line_thickness=line_thickness,
+        font_size=font_size,
+        left_margin=lm,
+        buttom_margin=bm,
+        top_margin=tm,
+        right_margin=rm,
+    )
     fig.write_image("plots/isoclines/niche_creation.svg")
 
 
 def niche_creation_cf():
-    Rs = np.linspace(0, 0.5, 10)
-    Ms = np.linspace(0, 0.5, 10)
-    Ts = np.linspace(0, p["M3"] / 20, 100)
+    Rs = np.linspace(1e-10, 0.5, 10)
+    Ms = np.linspace(1e-10, 0.5, 10)
+    Ts = np.linspace(1e-10, p["M3"] / 20, 100)
     R_grid, M_grid, T_grid = np.meshgrid(Rs, Ms, Ts)
     JCt = p["v1_1"] * R_grid / (R_grid + p["K1_1"]) + p["v1_2"] * M_grid / (
         M_grid + p["K1_2"]
     )
     JOa = p["v2_1"] * R_grid / (R_grid + p["K2_1"]) * T_grid / (T_grid + p["K2_3"])
-
+    J_ratio = JOa / JCt
+    custom_colorscale = [
+        [0, "lightblue"],  # Min value -> Blue
+        [0.5, "white"],  # Mid value (zero) -> White
+        [1, "lightsalmon"],  # Max value -> Red
+    ]
     fig = go.Figure()
-    fig.add_trace(
-        go.Isosurface(
-            x=R_grid.flatten(),
-            y=M_grid.flatten(),
-            z=T_grid.flatten(),
-            value=JCt.flatten(),
-            isomin=0.15,
-            isomax=0.15,
-            surface_count=1,
-            colorscale=[[0, colors["ct"]], [1, colors["ct"]]],
-            name="<i>Ct</i>",
-            showscale=False,
-            showlegend=True,
-            opacity=0.5,
-        )
-    )
     fig.add_trace(
         go.Isosurface(
             x=R_grid.flatten(),
@@ -247,6 +264,22 @@ def niche_creation_cf():
             surface_count=1,
             colorscale=[[0, colors["oa"]], [1, colors["oa"]]],
             name="<i>Oa</i>",
+            showlegend=True,
+            showscale=False,
+            opacity=0.5,
+        ),
+    )
+    fig.add_trace(
+        go.Isosurface(
+            x=R_grid.flatten(),
+            y=M_grid.flatten(),
+            z=T_grid.flatten(),
+            value=JCt.flatten(),
+            isomin=0.15,
+            isomax=0.15,
+            surface_count=1,
+            colorscale=[[0, colors["ct"]], [1, colors["ct"]]],
+            name="<i>Ct</i>",
             showlegend=True,
             showscale=False,
             opacity=0.5,
@@ -275,3 +308,8 @@ def niche_creation_cf():
     )
     fig = style_plot(fig, top_margin=0, left_margin=0, buttom_margin=0, font_size=7)
     fig.write_image("plots/isoclines/niche_creation_cf.svg")
+
+
+competition()
+mutual_cf()
+niche_creation()
